37af8e1c5c17e9332ccc12bc68a061d877413da1
dip-kush <dip.kush2008@gmail.com>
2015-04-15 23:34:46 +0530

    change in mul_inv

--- code.c
+++ code.c
@@ -822,7 +822,7 @@ int mul_inv(int a, int b)
         int t, nt, r, nr, q, tmp;
         if (b < 0) b = -b;
         if (a < 0) a = b - (-a % b);
- 																																																																																																																																																																																																																																																																																																																																																																				       t = 0;  nt = 1;  r = b;  nr = a % b;
+ 		t = 0;  nt = 1;  r = b;  nr = a % b;																																																																																																																																																																																																																																																																																																																																																																		       t = 0;  nt = 1;  r = b;  nr = a % b;
         while (nr != 0) {
           q = r/nr;
           tmp = nt;  nt = t - q*nt;  t = tmp;
